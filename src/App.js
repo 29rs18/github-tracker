d758c68b259e3a00e1d0743e5a7d63e613f7d6dc
29rs18 <29rs18@gmail.com>
2021-05-01 06:22:33 +0530

    github tracker

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React,{Component, Fragment} from 'react';
+import { render } from 'react-dom';
+import './App.css';
+import {BrowserRouter as Router,Switch,Route}from 'react-router-dom';
+import Navbar from './components/layout/navbar'
+import UserItem from './components/users/UserItem'
+import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/Search'
+import Alert  from './components/layout/Alert';
+import About  from './components/pages/About'
+class App extends Component {
+  state={
+    users:[],
+    loading:false,
+    alert:null,
+  }
+  async componentDidMount(){
+    this.setState({loading:true});
+     const res=await axios.get('https://api.github.com/users');
+     this.setState({users:res.data,loading:false});
+     
+  }
+  //search github users
+  searchUsers=async(text)=>{
+    this.setState({loading:true});
+    const res=await axios.get(`https://api.github.com/search/users?q=${text}`);
+    this.setState({users:res.data.items,loading:false});
+  }
+  //clear users from state
+  clearUsers=()=>{
+    this.setState({users:[],loading:false});
+  }
+  setAlert=(msg,type)=>{
+     this.setState({alert:{msg:msg,type:type}} );
+      setTimeout(()=>this.setState({alert:null}),5000);
+  }
+  render() {
+  return (
+    <Router>
+    <div className="App">
+      <Navbar title ='Github Finder' icon ='fab fa-github'/>
+      
+      <div className='container'>
+      <Switch>
+          <Route exact path='/' render ={props=>(
+            <Fragment>
+              <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length>0?true :false} setAlert={this.setAlert}></Search>
+        <Users loading={this.state.loading} users={this.state.users}/>
+
+            </Fragment>
+
+          )}/>
+          <Route exact path='/about' component={About}></Route>
+              </Switch>
+      
+        </div>
+       
+    </div>
+    </Router>
+  );
+}
+}
+
+export default App;
